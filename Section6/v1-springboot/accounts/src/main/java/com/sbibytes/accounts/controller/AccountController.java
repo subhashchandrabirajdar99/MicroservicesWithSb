273523f4afea17bfbb62653defc808c99713b2c8
Subhash Birajdar <subhashchandrabirajdar99@gmail.com>
2026-02-23 12:41:50 -0800

    section 6 : Account ms used Config for Environment variable

--- Section6/v1-springboot/accounts/src/main/java/com/sbibytes/accounts/controller/AccountController.java
+++ Section6/v1-springboot/accounts/src/main/java/com/sbibytes/accounts/controller/AccountController.java
@@ -15,7 +15,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -39,6 +41,9 @@ public class AccountController {
 
     @Value("${build.version}")
     private String buildVersion;
+
+    @Autowired
+    private Environment environment;
 
     @Operation(
             summary = "Create Account REST API",
@@ -176,5 +181,28 @@ public class AccountController {
                 body(buildVersion);
     }
 
-
+    @Operation(
+            summary = "Get Java Version",
+            description = "Get Java Version details that is installed into accounts microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion(){
+        return ResponseEntity
+                .status(HttpStatus.OK).
+                body(environment.getProperty("JAVA_HOME"));
+    }
 }
